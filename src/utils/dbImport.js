9022c632932ab02cba2c0e7752ef2d76233c4249
Zalbani <pro.pierson.alban@gmail.com>
2020-07-10 23:00:38 +0200

    Update url dynamically

--- src/utils/dbImport.js
+++ src/utils/dbImport.js
@@ -3,7 +3,8 @@ import db from '~/utils/db';
 
 export default async function importdb() {
   await db.delete();
-  const url = 'http://localhost:8082/data-export.json';
+  const baseUrl = window.location.origin;
+  const url = `${baseUrl}/data-export.json`;
   const blob = await fetch(url).then((r) => r.blob());
   await importDB(blob);
   document.location.reload(true);
