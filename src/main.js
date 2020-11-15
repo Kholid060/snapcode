2777279a6cc001b8d07d2033fdf7fb142434f768
Ahmad Kholid <kholid060@gmail.com>
2020-11-15 15:55:32 +0800

    add service worker

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store';
 import App from './App.vue';
 import icons from './lib/icons';
 import VAutoresize from './directives/VAutoresize';
+import './registerServiceWorker';
 import './assets/css/tailwind.css';
 import './assets/css/style.css';
 import 'comps-ui/dist/style.css';
