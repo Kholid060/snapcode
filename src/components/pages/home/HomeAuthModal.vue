cf2ec436ac193ecc76013f44b8987c877c53a60c
Ahmad Kholid <kholid060@gmail.com>
2021-06-01 14:57:44 +0800

    feat: add github auth

--- src/components/pages/home/HomeAuthModal.vue
+++ src/components/pages/home/HomeAuthModal.vue
@@ -3,14 +3,17 @@
     <template #header> Login </template>
     <div class="space-y-2 mt-2">
       <button
+        v-for="(provider, id) in providers"
+        :key="id"
+        :style="{ backgroundColor: provider.color }"
         class="flex items-center px-4 w-full rounded-lg"
-        style="height: 52px; background-color: #4285f4"
-        @click="loginWith('google.com')"
+        style="height: 52px"
+        @click="loginWith(id)"
       >
-        <div class="p-1 rounded-lg bg-white">
-          <img :src="googleLogoSvg" class="h-6 w-6" />
+        <div class="p-1 rounded-full bg-white">
+          <img :src="provider.logo" class="h-6 w-6" />
         </div>
-        <span class="ml-3">Continue with Google</span>
+        <span class="ml-3">Continue with {{ provider.name }}</span>
       </button>
     </div>
   </modal-ui>
@@ -20,6 +23,7 @@ import { ref } from 'vue';
 import emitter from 'tiny-emitter/instance';
 import { auth } from '~/utils/firebase';
 import googleLogoSvg from '~/assets/svg/google-logo.svg';
+import githubLogoSvg from '~/assets/svg/github-logo.svg';
 
 export default {
   props: {
@@ -27,13 +31,24 @@ export default {
   },
   emits: ['update:modelValue'],
   setup(props, { emit }) {
+    const providers = {
+      'google.com': {
+        name: 'Google',
+        logo: googleLogoSvg,
+        color: '#4285f4',
+        scope: 'https://www.googleapis.com/auth/userinfo.profile',
+      },
+      'github.com': {
+        name: 'GitHub',
+        logo: githubLogoSvg,
+        color: '#24292e',
+      },
+    };
+
     const show = ref(false);
 
     function loginWith(provider) {
-      auth.signInWithProvider({
-        provider,
-        oauthScope: 'https://www.googleapis.com/auth/userinfo.profile',
-      });
+      auth.signInWithProvider(provider);
     }
 
     emitter.on('show-auth', (value = true) => {
@@ -42,6 +57,7 @@ export default {
 
     return {
       show,
+      providers,
       loginWith,
       googleLogoSvg,
     };
