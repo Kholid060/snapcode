f8f2d64df3c3c4f6279d572857997e1426b8b9eb
Ahmad Kholid <kholid060@gmail.com>
2021-07-15 07:12:34 +0800

    fix: auth modal not showing up on explore route

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -14,7 +14,6 @@
         </div>
       </div>
     </div>
-    <home-auth-modal></home-auth-modal>
     <home-gists-modal></home-gists-modal>
   </div>
 </template>
@@ -22,14 +21,12 @@
 import { onMounted } from 'vue';
 import HomeSnippets from '~/components/pages/home/HomeSnippets.vue';
 import HomeNavigation from '~/components/pages/home/HomeNavigation.vue';
-import HomeAuthModal from '~/components/pages/home/HomeAuthModal.vue';
 import HomeGistsModal from '~/components/pages/home/HomeGistsModal.vue';
 import backup from '~/utils/backup';
 
 export default {
   components: {
     HomeSnippets,
-    HomeAuthModal,
     HomeNavigation,
     HomeGistsModal,
   },
