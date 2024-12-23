b063dcbfdda7bf9f6056a5a43b115d7a8bf5fc7b
Ahmad Kholid <kholid060@gmail.com>
2024-12-23 17:02:27 +0800

    feat(ui): add tags input component

---
+++ packages/ui/src/components/ui/tags-input/TagsInputItemText.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { cn } from '../../../lib/utils';
+import {
+  TagsInputItemText,
+  type TagsInputItemTextProps,
+  useForwardProps,
+} from 'radix-vue';
+import { computed, type HTMLAttributes } from 'vue';
+
+const props = defineProps<
+  TagsInputItemTextProps & { class?: HTMLAttributes['class'] }
+>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <TagsInputItemText
+    v-bind="forwardedProps"
+    :class="cn('rounded bg-transparent px-2 py-1 text-sm', props.class)"
+  />
+</template>
