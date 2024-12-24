ab12be17a023353c1d4953df2c88df56379f96b1
Ahmad Kholid <kholid060@gmail.com>
2024-12-24 16:56:03 +0800

    feat(ui): add accordion component

---
+++ packages/ui/src/components/ui/accordion/AccordionContent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { cn } from '../../../lib/utils';
+import { AccordionContent, type AccordionContentProps } from 'radix-vue';
+import { computed, type HTMLAttributes } from 'vue';
+
+const props = defineProps<
+  AccordionContentProps & { class?: HTMLAttributes['class'] }
+>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+</script>
+
+<template>
+  <AccordionContent
+    v-bind="delegatedProps"
+    class="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm transition-all"
+  >
+    <div :class="cn('pb-4 pt-0', props.class)">
+      <slot />
+    </div>
+  </AccordionContent>
+</template>
---
+++ packages/ui/src/components/ui/skeleton/Skeleton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { cn } from '../../../lib/utils';
+
+interface SkeletonProps {
+  class?: HTMLAttributes['class'];
+}
+
+const props = defineProps<SkeletonProps>();
+</script>
+
+<template>
+  <div :class="cn('bg-muted animate-pulse rounded-md', props.class)" />
+</template>
