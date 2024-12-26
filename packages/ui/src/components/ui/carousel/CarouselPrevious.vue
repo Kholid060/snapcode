53202f79f0ebc254e337c40bcdf3b30a9f83e843
Ahmad Kholid <kholid060@gmail.com>
2024-12-26 16:49:36 +0800

    feat(ui): add carousel component

---
+++ packages/ui/src/components/ui/carousel/CarouselPrevious.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { WithClassAsProps } from './interface';
+import { Button } from '../button';
+import { cn } from '../../../lib/utils';
+import { ArrowLeft } from 'lucide-vue-next';
+import { useCarousel } from './useCarousel';
+
+const props = defineProps<WithClassAsProps>();
+
+const { orientation, canScrollPrev, scrollPrev } = useCarousel();
+</script>
+
+<template>
+  <Button
+    :disabled="!canScrollPrev"
+    :class="
+      cn(
+        'absolute h-8 w-8 touch-manipulation rounded-full p-0',
+        orientation === 'horizontal'
+          ? '-left-12 top-1/2 -translate-y-1/2'
+          : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
+        props.class,
+      )
+    "
+    variant="outline"
+    @click="scrollPrev"
+  >
+    <slot>
+      <ArrowLeft class="h-4 w-4 text-current" />
+      <span class="sr-only">Previous Slide</span>
+    </slot>
+  </Button>
+</template>
