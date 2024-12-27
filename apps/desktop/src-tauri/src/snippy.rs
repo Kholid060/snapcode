88965f74a5e299e824ba0f9c26d1e774d43a4569
Ahmad Kholid <kholid060@gmail.com>
2024-12-27 09:51:24 +0800

    fix(desktop): autostart argument

--- apps/desktop/src-tauri/src/snippy.rs
+++ apps/desktop/src-tauri/src/snippy.rs
@@ -1,16 +1,16 @@
 use tauri::Manager;
 
-pub mod tray;
-pub mod window;
-pub mod snippet;
 pub mod document;
 pub mod keyboard;
+pub mod snippet;
+pub mod tray;
+pub mod window;
 
 pub fn init_app(app: &mut tauri::App) -> tauri::Result<()> {
     tray::init_app_tray(app)?;
     document::init_app_document(app)?;
 
-    if !std::env::args().any(|arg| &arg == "autostart") {
+    if !std::env::args().any(|arg| &arg == "--autostart") {
         window::MainWindow::create_or_show(app.app_handle())?;
     }
 
